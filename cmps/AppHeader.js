33b623c3ab5f4d7370063058304f25647053ecca
Don Nissan <donnynis@gamil.com>
2023-03-01 20:44:31 +0200

    main nav layout

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -3,10 +3,10 @@ export default {
         <header class="app-header">
             <h1>AppSus</h1>
             <nav>
-                <router-link to="/">Home</router-link> |
-                <router-link to="/about">About</router-link> |
-                <router-link to="/email">Email</router-link> |
-                <router-link to="/keep">Keep</router-link>
+                <router-link class="nav-link" to="/">Home</router-link> |
+                <router-link class="nav-link" to="/about">About</router-link> |
+                <router-link class="nav-link" to="/email">Email</router-link> |
+                <router-link class="nav-link" to="/keep">Keep</router-link>
             </nav>
         </header>
     `,
