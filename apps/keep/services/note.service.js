b0a4188f287c109ecda894f93b55b5203c89abcd
Daniel Krot <danielkr1324@gmail.com>
2023-03-01 12:33:07 +0200

    Basic routing and service build

---
+++ apps/keep/services/note.service.js
@@ -0,0 +1,57 @@
+import { utilService } from '../../../services/util.service'
+import { storageService } from '../../../services/async-storage.service'
+
+const NOTES_KEY = 'notesDB'
+
+_createNotes()
+
+function _createNotes() {
+  let notes = utilService.loadFromStorage(NOTES_KEY)
+  if (!notes || !notes.length) {
+    notes = [
+      {
+        id: 'n101',
+        createdAt: 1112222,
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+          backgroundColor: '#00d',
+        },
+        info: {
+          txt: 'Fullstack Me Baby!',
+        },
+      },
+      {
+        id: 'n102',
+        type: 'NoteImg',
+        isPinned: false,
+        info: {
+          url: 'http://some-img/me',
+          title: 'Bobi and Me',
+        },
+        style: {
+          backgroundColor: '#00d',
+        },
+      },
+      {
+        id: 'n103',
+        type: 'NoteTodos',
+        isPinned: false,
+        info: {
+          title: 'Get my stuff together',
+          todos: [
+            { txt: 'Driving license', doneAt: null },
+            { txt: 'Coding power', doneAt: 187111111 },
+          ],
+        },
+      },
+    ]
+    utilService.saveToStorage(NOTES_KEY, notes)
+  }
+}
+
+function query() {
+    return storageService.query(NOTES_KEY)
+}
+
+
