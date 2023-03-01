23a3a04e32a244fc63b15f59a8a70f54333dd926
Daniel Krot <danielkr1324@gmail.com>
2023-03-01 13:53:48 +0200

    test

--- routes.js
+++ routes.js
@@ -1,11 +1,7 @@
 import HomePage from './views/HomePage.js'
-<<<<<<< HEAD
-// import AboutUs from './views/AboutUs.js'
-=======
 import AboutUs from './views/AboutUs.js'
->>>>>>> 347937114194b20443c52ff26dfbd4082aee0b49
 // import EmailIndex from './apps/mail/pages/EmailIndex.js'
-// import NoteIndex from './apps/keep/pages/NoteIndex.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -16,25 +12,18 @@ const routerOptions = {
       path: '/',
       component: HomePage,
     },
-<<<<<<< HEAD
-    // {
-    //   path: '/about',
-    //   component: AboutUs,
-    // },
-=======
     {
       path: '/about',
       component: AboutUs,
     },
->>>>>>> 347937114194b20443c52ff26dfbd4082aee0b49
     // {
     //   path: '/email',
     //   component: EmailIndex,
     // },
-    // {
-    //   path: '/keep',
-    //   component: NoteIndex,
-    // },
+    {
+      path: '/keep',
+      component: NoteIndex,
+    },
   ],
 }
 
