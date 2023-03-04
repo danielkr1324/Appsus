ac6bc8444fbc6b231e805d88473cc345424bb492
Daniel Krot <danielkr1324@gmail.com>
2023-03-04 20:16:11 +0200

    further styling

--- apps/mail/cmps/EmailPreview.js
+++ apps/mail/cmps/EmailPreview.js
@@ -1,48 +1,55 @@
 export default {
-    props: ['email'],
+  props: ['email'],
 
-    template: `
+  template: `
         <section>
         <RouterLink :to="'/email/'+email.id" :style="isRead" class="email-preview" >
             <main class="preview-main">
             <div class="preview-from"> {{ email.from }} </div> 
             <div class="preview-subject"> {{ email.subject }} &nbsp;|&nbsp; {{ email.body }} </div>
             <!-- <div class="preview-time" :sentAt="convertDate(email.sentAt)"> {{sentAt}} </div> -->
+            <button class="btn-email-remove" @click.stop="removeEmail(email.id)">  <i class="fa-regular fa-trash-can"></i> </button>
         </main>
         </RouterLink>
-        <!-- <button class="btn-email-remove" @click="removeEmail(email.id)"> X </button> -->
         </section>
     `,
 
-    data() {
-        return {
-            sentAt: null
-        }
-    },
+  data() {
+    return {
+      sentAt: null,
+    }
+  },
 
-    methods: {
-        removeEmail(emailId) {
-            this.$emit('emailRemoved', emailId)
-        }
+  methods: {
+    removeEmail(emailId) {
+      this.$emit('emailRemoved', emailId)
     },
+  },
 
-    computed: {
-        isRead() {
-            return {
-                fontWeight: (this.email.isRead) ? 'normal' : 'bold'
-            }
-        },
-        convertDate(timeStamp) {
-            console.log('timeStamp : ', timeStamp)
-            var date = new Date(timeStamp * 1000);
-            console.log("Unix Timestamp:", timeStamp)
-            console.log("Date Timestamp:", date.getTime())
-            console.log(timeStamp)
-            console.log("Date: " + date.getDate() +
-                "/" + (date.getMonth() + 1) +
-                "/" + date.getFullYear() +
-                " " + date.getHours() +
-                ":" + date.getMinutes());
-        }
+  computed: {
+    isRead() {
+      return {
+        fontWeight: this.email.isRead ? 'normal' : 'bold',
+      }
     },
+    convertDate(timeStamp) {
+      console.log('timeStamp : ', timeStamp)
+      var date = new Date(timeStamp * 1000)
+      console.log('Unix Timestamp:', timeStamp)
+      console.log('Date Timestamp:', date.getTime())
+      console.log(timeStamp)
+      console.log(
+        'Date: ' +
+          date.getDate() +
+          '/' +
+          (date.getMonth() + 1) +
+          '/' +
+          date.getFullYear() +
+          ' ' +
+          date.getHours() +
+          ':' +
+          date.getMinutes()
+      )
+    },
+  },
 }
--- apps/mail/pages/EmailDetails.js
+++ apps/mail/pages/EmailDetails.js
@@ -10,7 +10,6 @@ export default {
         <EmailNav/>
         <div class="details-nav"> 
             <RouterLink to="/email"> <button><i class="fa-solid fa-arrow-left"></i> </button></RouterLink>
-            <button class="btn-email-remove" @click="removeEmail(email.id)"> <i class="fa-regular fa-trash-can"></i> </button>
         </div>
         <div class="email-body">
             <div class="details-subject"> {{ email.subject }} </div> 
