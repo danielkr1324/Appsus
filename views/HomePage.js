65547aeba731665b88b6277a8860dabfee9c2c6d
Daniel Krot <danielkr1324@gmail.com>
2023-03-05 10:40:16 +0200

    debugging

--- views/HomePage.js
+++ views/HomePage.js
@@ -1,8 +1,8 @@
 export default {
-    template: `
+  template: `
         <section class="home-page">
-            <RouterLink to="/email"> <img class="email" src="/assets/img/email.png" alt="Email"> </RouterLink>
-            <RouterLink to="/keep"> <img class="keep" src="/assets/img/keep.png" alt="Keep"> </RouterLink>
+            <RouterLink to="/email"> <img class="email" src="assets/img/email.png" alt="Email"> </RouterLink>
+            <RouterLink to="/keep"> <img class="keep" src="assets/img/keep.png" alt="Keep"> </RouterLink>
             <!-- <h1>Books</h1> -->
         </section>
     `,
